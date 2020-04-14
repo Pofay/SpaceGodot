302d024a1422f94370074b64c7f0612861d04ee6
Pofay <gianCarlo.gilos@gmail.com>
2020-04-14 10:39:55 +0800

    Added bullet movement

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,15 +1,19 @@
-extends RigidBody2D
+extends KinematicBody2D
+
 
 
 export var bulletSpeed = 600
-var velocity = Vector2()
+var velocity = Vector2(bulletSpeed, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print('Wassap!')
 	pass # Replace with function body.
 
 
-func _process(delta):
-	velocity.y += 1
-	velocity = velocity.normalized() * bulletSpeed
+
+func _physics_process(delta):
+	move_and_collide(velocity * delta)
 	pass
+
+
