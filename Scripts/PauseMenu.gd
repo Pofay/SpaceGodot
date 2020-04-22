6750652851b1c7d92f88e4acee7e8a662b1c8099
Pofay <gianCarlo.gilos@gmail.com>
2020-04-22 19:56:55 +0800

    Hooked Resume and Quit Buttons in Pause Menu to work as intended

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -8,3 +8,10 @@ func _input(event):
 		get_tree().paused = not get_tree().paused
 		visible = get_tree().paused
 
+func _on_ResumeButton_pressed():
+	get_tree().paused = not get_tree().paused
+	visible = get_tree().paused
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
